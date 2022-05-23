2ba4dcb1c4eed7760a8060d67c0a56d54cdf0416
RajatDiwate <rajat.diwate94@gmail.com>
2022-05-23 23:10:38 +0530

    maximum-triplet-sum-array

--- src/com/practice/SpiralMatrix16.java
+++ src/com/practice/SpiralMatrix16.java
@@ -6,12 +6,35 @@ public class SpiralMatrix16 {
                 {5, 6, 7, 8},
                 {9, 10, 11, 12},
                 {13, 14, 15, 16}};
+        //Output: 1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10
+        int m = arr.length;
+        int n = arr[0].length;
+        int k = 0, l = 0;
+        int i;
+        while (k < m && l < n) {
+            for (i = l; i < m; i++) {
+                System.out.print(arr[k][i] + " ");
+            }
+            k++;
+            for (i = k; i < m; i++) {
+                System.out.print(arr[i][n - 1] + " ");
+            }
+            n--;
+            if (k < m) {
+                for (i = n - 1; i >= l; --i) {
+                    System.out.print(arr[m - 1][i] + " ");
+                }
+                m--;
+            }
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                System.out.print(arr[i][j]);
-            }            
+            // Print the first column from the remaining
+            // columns */
+            if (l < n) {
+                for (i = m - 1; i >= k; --i) {
+                    System.out.print(arr [i][l] + " ");
+                }
+                l++;
+            }
         }
-        
     }
 }
